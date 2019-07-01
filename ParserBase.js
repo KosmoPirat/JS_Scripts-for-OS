4648f411a4a696ba5d08079db4fd1ab8053ec2c4
KosmoPirat <kosmopirat@gmail.com>
2019-07-01 17:15:28 +0300

    Initial commit

---
+++ ParserBase.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+const request = require('request');
+var http = require('http');
+
+class Parser {
+    constructor(urlFrom, urlTo,) {
+        this.urlFrom = urlFrom;
+        this.urlTo = urlTo;
+    }
+
+    async getHtmlRequest(urlFrom, options) {
+        return fetch(urlFrom, options).then(res => res.json());
+    }
+
+    downloadFiles(url, filename) {
+         request.get(url)
+            .on('response',  (response) => {
+                let file = fs.createWriteStream(`./convert/${filename}`);
+                console.log(response.statusCode);
+                console.log(response.headers['content-type']);
+                response.pipe(file);
+                file.on('finish', () => file.close())
+            })
+            .on('error', (err) => {
+                console.error(`Ошибка запроса, url: ${url}. ${err}`);
+                fs.unlinkSync(`./convert/${filename}`);
+            })
+    }
+        /*request(url)
+            .pipe(fs.createWriteStream(`./convert/${filename}`))
+            //.on('error', err => console.log(`file didn't save: ${err};\n`));
+            .on('close', filename => console.log(`save file: ${filename};\n`))
+
+    }*/
+
+    writeData(data, urlTo) {
+        fs.writeFile(urlTo, data, error => {
+                if (error) {
+                    console.log(`Ошибка записи в  файт writeData(). Error code: ${error}`)
+                } else {
+                    console.log("Запись файла завершена успешно!");
+                }
+            }
+        );
+    }
+}
+
+module.exports = Parser;
